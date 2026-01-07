92c10a3981d967653f83d49ec415776343afde32
Wahab Ali <wahabalimk@gmail.com>
2026-01-07 18:38:20 -0500

    fix: code review suggestions
    Signed-off-by: Wahab Ali <wahabalimk@gmail.com>

--- internal/controller/multiclusterservice_controller.go
+++ internal/controller/multiclusterservice_controller.go
@@ -723,7 +723,7 @@ func (r *MultiClusterServiceReconciler) okToReconcileServiceSet(ctx context.Cont
 		}
 
 		selfMgmtDependency := mcs.Spec.ServiceSpec.Provider.SelfManagement && depMCS.Spec.ServiceSpec.Provider.SelfManagement
-		if !sel.Matches(labels.Set(clusterLabels)) && !selfMgmtDependency {
+		if !selfMgmtDependency && !sel.Matches(labels.Set(clusterLabels)) {
 			// depMCS does not match the provided CD via labels but before continuing
 			// we still have to see whether both mcs and depMCS manage the mothership.
 			// If they do then we will have to consider the status of depMCS's services.
