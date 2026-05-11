5b49908f8147739833bf0b75901fe81ac0af3b06
Wahab Ali <wahabalimk@gmail.com>
2026-05-11 11:52:53 -0400

    chore: Bump sveltos to 1.9.0 & handle scheme.Builder deprecation
    Signed-off-by: Wahab Ali <wahabalimk@gmail.com>

--- api/v1beta1/groupversion_info.go
+++ api/v1beta1/groupversion_info.go
@@ -18,16 +18,31 @@
 package v1beta1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
+
+// localSchemeBuilder wraps runtime.SchemeBuilder and provides the Register(runtime.Object...)
+// calling convention used by each type's init() function in this package.
+type localSchemeBuilder struct {
+	runtime.SchemeBuilder
+}
+
+func (b *localSchemeBuilder) Register(objects ...runtime.Object) {
+	b.SchemeBuilder.Register(func(s *runtime.Scheme) error {
+		s.AddKnownTypes(GroupVersion, objects...)
+		metav1.AddToGroupVersion(s, GroupVersion)
+		return nil
+	})
+}
 
 var (
 	// GroupVersion is group version used to register these objects.
 	GroupVersion = schema.GroupVersion{Group: "k0rdent.mirantis.com", Version: "v1beta1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &localSchemeBuilder{}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
